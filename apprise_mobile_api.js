3f5d432fcd85294af117b0aa68bd8bc778a579dc
Ed Reel <edreel@gmail.com>
2017-10-17 00:09:41 -0500

    Add logic to push content to APPrise immediately upon submit
      - Add jQuery code to default the Unpost date to 15 days from the Post date
      - Remove default Post and Unpost dates from field definitions
      - Allow empty Post and Unpost date fields
      - Add date contrib module dependency
      - Clean up to conform to Drupal coding standards

---
+++ apprise_mobile_api.js
@@ -0,0 +1,32 @@
+(function ($, Drupal) {
+Drupal.behaviors.appriseMobileApi = {
+  attach: function (context, settings) {
+    $('input[name="field_apprise_mobile_api_post[und][0][value][date]"]').change(function() {
+      setDate();
+    });
+    $('input[name="field_apprise_mobile_api_post[und][0][value][time]"]').change(function() {
+      setDate();
+    });
+    function setDate() {
+      var date = $('input[name="field_apprise_mobile_api_post[und][0][value][date]"]').val();
+      var time = $('input[name="field_apprise_mobile_api_post[und][0][value][time]"]').val();
+      var datetime = Date.parse(date + ' ' + time);
+      var plus15days = datetime + (15 * 24 * 60 * 60 * 1000);
+      var newdatetime = new Date(plus15days);
+      var d = newdatetime.getDate();
+      var m = newdatetime.getMonth() + 1;
+      var y = newdatetime.getFullYear();
+      var newdate = m + '/' + d + '/' + y;
+      var h = newdatetime.getHours();
+      var n = newdatetime.getMinutes();
+      var newtime = pad(h, 2) + ':' + pad(n, 2);
+      $('input[name="field_apprise_mobile_api_unpost[und][0][value][date]"]').val(newdate);
+      $('input[name="field_apprise_mobile_api_unpost[und][0][value][time]"]').val(newtime);
+    }
+    function pad(str, max) {
+      str = str.toString();
+      return str.length < max ? pad("0" + str, max) : str;
+    }
+  }
+};
+}(jQuery, Drupal));
